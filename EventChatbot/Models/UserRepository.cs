416e435d8caa6cef786ac39d013bb590d235df4f
rhmsarr <sarr.rahma@gmx.com>
2024-12-01 18:53:02 +0300

    unified the interface. need to take a quick look at modals and navbar collapse

--- EventChatbot/Models/UserRepository.cs
+++ EventChatbot/Models/UserRepository.cs
@@ -7,31 +7,7 @@ namespace EventChatbot.Models{
             user = new User();
             user = u;
         }
-        public static void InitializeWeek(){
-            
-            if(user.Availability.Monday == null){
-                user.Availability.Monday = new Day();
-            }
-            if(user.Availability.Tuesday == null){
-                user.Availability.Tuesday = new Day();
-            }
-            if(user.Availability.Wednesday == null){
-                user.Availability.Wednesday = new Day();
-            }
-            if(user.Availability.Thursday == null){
-                user.Availability.Thursday = new Day();
-            }
-            if(user.Availability.Friday == null){
-                user.Availability.Friday = new Day();
-            }
-            if(user.Availability.Saturday == null){
-                user.Availability.Saturday = new Day();
-            } 
-            if(user.Availability.Sunday == null){
-                user.Availability.Sunday = new Day();
-            }
-          
-        }
+        
         
     }
 }
