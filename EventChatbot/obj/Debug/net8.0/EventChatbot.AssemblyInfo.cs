cfcf95f629620acfff7f859e5a4302a243302b78
aloramercer <beyzakomis@gmail.com>
2024-12-09 19:25:31 +0300

    Save local changes

--- EventChatbot/obj/Debug/net8.0/EventChatbot.AssemblyInfo.cs
+++ EventChatbot/obj/Debug/net8.0/EventChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f4ab619e0fea3046025f86a0fea5da1db739fd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9890b1de3f6c702b1dcde22c1f9ae9a51f517c32")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
